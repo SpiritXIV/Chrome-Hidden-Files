7f2388fd29f96df703a6c52e43bf735e481caf59
Spirit XIV <85543096+SpiritXIV@users.noreply.github.com>
2022-11-29 01:15:23 -0600

    Update funny.js

--- www/funny.js
+++ www/funny.js
@@ -1,19 +1,96 @@
-<script>
-// Configure the blocker here:
-var stalker = {
-  forward: 'https://weareerror404.com/',
-  ip_addresses: ['127.0.0.1'],
-  whitelist_ip_addresses: ['127.0.0.2'],
-  cities: ['Seattle', 'Olympia'],
-  states: ['OR'],
-  proxies: true
-};
-</script>
+/* ------------------------------ */
+/* FREE IP INFO API THIRD PARTIES */
+/* ------------------------------ */
+// -> https://ipapi.co/json
+// -> https://ipinfo.io/json
+// -> https://get.geojs.io/v1/ip/country.json
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script>
-// stalker.js
-function proxyBlock(a){document.URL!=stalker.forward&&"Y"==a.proxy&&(window.location=stalker.forward)}
-function jsStalkerGet(a){document.URL==stalker.forward||(-1==stalker.ip_addresses.indexOf(a.geoplugin_request)&&-1==stalker.cities.indexOf(a.geoplugin_city)&&-1==stalker.states.indexOf(a.geoplugin_region)||-1!=stalker.whitelist_ip_addresses.indexOf(a.geoplugin_request))||(window.location=stalker.forward);stalker.proxies&&$.ajax({url:"http://4gods.nl/~nik/proxyblock.php",type:"GET",data:{ip:a.geoplugin_request,format:"jsonp"},crossDomain:!0,dataType:"jsonp",jsonp:"cb",jsonpCallback:"proxyBlock"})}
-</script>
-<script src="http://www.geoplugin.net/json.gp?jsoncallback=jsStalkerGet" type="application/javascript"></script>
+/* ------------------- */
+/* ACCESS DENIED ERROR */
+/* ------------------- */
+function display_access_denied_error() {
+    document.body.innerHTML
+        = '<div id="access-denied-error">'
+        + '<div class="middle-center">'
+        + '<span class="pulsate-bck">'
+        + '<i class="bi bi-exclamation-diamond-fill"></i>'
+        + 'Access Denied'
+        + '</span>'
+        + '</div>'
+        + '</div>'
+}
+
+/* ------------------------- */
+/* BLOCK BLACKLIST COUNTRIES */
+/* ------------------------- */
+function block_blacklist_countries() {
+    // Blacklist countries
+    const blacklist_countries = [
+        "DE", // Germany
+        "US", // United States
+        "GB", // United Kingdom
+        "UA", // Ukraine
+        "AR", // Argentina
+        "FI", // Finland
+        "CA", // Canada
+        "JP", // Japan
+        "NO", // Norway
+        "RU" // Russia
+    ]
+
+    // Detecting the users country
+    function get_country_code(api_url) {
+        fetch(api_url, { method: 'GET' })
+            .then(response => response.json()) // Getting ip info as json
+            .then(result => {
+                if (blacklist_countries.includes(result.country)) { // If my ip country code is in blacklist
+                    display_access_denied_error() // Access denied error
+                }
+            })
+            .catch(error => console.log('error', error))
+    }
+
+    // Getting country code from third party api
+    get_country_code("https://get.geojs.io/v1/ip/country.json")
+}
+
+/* ------------------------- */
+/* ALLOW WHITELIST COUNTRIES */
+/* ------------------------- */
+function allow_whitelist_countries() {
+    // Whitelist countries
+    const whitelist_countries = [
+        "DE", // Germany
+        "US", // United States
+        "GB", // United Kingdom
+        "UA", // Ukraine
+        "AR", // Argentina
+        "FI", // Finland
+        "CA", // Canada
+        "JP", // Japan
+        "NO", // Norway
+        "RU" // Russia
+    ]
+
+    // Detecting the users country
+    function get_country_code(api_url) {
+        fetch(api_url, { method: 'GET' })
+            .then(response => response.json()) // Getting ip info as json
+            .then(result => {
+                if (!whitelist_countries.includes(result.country)) { // If my ip country code is not in whitelist
+                    display_access_denied_error() // Access denied error
+                }
+            })
+            .catch(error => console.log('error', error))
+    }
+
+    // Getting country code from third party api
+    get_country_code("https://get.geojs.io/v1/ip/country.json")
+}
+
+/* -------------- */
+/* CALL FUNCTIONS */
+/* -------------- */
+block_blacklist_countries() // Block blacklist countries
+
+// allow_whitelist_countries() // Allow whitelist countries
