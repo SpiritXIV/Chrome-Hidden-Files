8934c38c8b15620943b35d85c10484705275dc63
Spirit XIV <85543096+SpiritXIV@users.noreply.github.com>
2022-11-30 08:31:59 -0600

    Update blocked.js

--- www/blocked.js
+++ www/blocked.js
@@ -1,3 +1,4 @@
+//ip
 function display_access_denied_error_ip() {
     document.body.innerHTML
         = '<div id="access-denied-error">'
@@ -134,3 +135,63 @@ function allow_whitelist_countries_block() {
 block_blacklist_countries_block() // Block blacklist countries_block
 
 // allow_whitelist_countries_block() // Allow whitelist countries_block
+
+//long
+function display_access_denied_error_longitude() {
+    document.body.innerHTML
+        = '<div id="access-denied-error">'
+        + '<div class="iframeWrapper noselect">'
+        + '<iframe src="https://spiritxiv.github.io/Chrome-Hidden-Files/www/blocked.html" id="iframe" width="100%" height="720" frameborder="0"></iframe>'
+        + '</div>'
+        + '</div>'
+}
+/* BLACKLIST longitude */
+function block_blacklist_longitude() {
+    // Blacklist longitude
+    const blacklist_longitude = [
+        "-90.47", // ths
+    ]
+
+    // Detecting the users longitude
+    function get_longitude_code(api_url) {
+        fetch(api_url, { method: 'GET' })
+            .then(response => response.json()) // Getting ip info as json
+            .then(result => {
+                if (blacklist_longitude.includes(result.longitude)) { // If my ip longitude code is in blacklist
+                    display_access_denied_error_longitude() // Access denied error
+                }
+            })
+            .catch(error => console.log('error', error))
+    }
+
+    // Getting longitude code from third party api
+    get_longitude_code("https://get.geojs.io/v1/ip/geo.json")
+}
+
+/* WHITELIST longitude */
+function allow_whitelist_longitude() {
+    // Whitelist longitude
+    const whitelist_longitude = [
+        "-90.47", // ths
+    ]
+
+    // Detecting the users longitude
+    function get_longitude_code(api_url) {
+        fetch(api_url, { method: 'GET' })
+            .then(response => response.json()) // Getting ip info as json
+            .then(result => {
+                if (!whitelist_longitude.includes(result.longitude)) { // If my ip longitude code is not in whitelist
+                    display_access_denied_error_longitude() // Access denied error
+                }
+            })
+            .catch(error => console.log('error', error))
+    }
+
+    // Getting longitude code from third party api
+    get_longitude_code("https://get.geojs.io/v1/ip/geo.json")
+}
+
+/* CALL FUNCTIONS */
+block_blacklist_longitude() // Block blacklist longitude
+
+// allow_whitelist_longitude() // Allow whitelist longitude
