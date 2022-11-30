fb07c68d3662d8d5569cdb44f9e851ac5dbb6e9f
Spirit XIV <85543096+SpiritXIV@users.noreply.github.com>
2022-11-30 08:41:23 -0600

    Update blocked.js

--- www/blocked.js
+++ www/blocked.js
@@ -149,7 +149,7 @@ function display_access_denied_error_longitude() {
 function block_blacklist_longitude() {
     // Blacklist longitude
     const blacklist_longitude = [
-        "-90.47", // ths
+        "", // test
     ]
 
     // Detecting the users longitude
@@ -172,7 +172,7 @@ function block_blacklist_longitude() {
 function allow_whitelist_longitude() {
     // Whitelist longitude
     const whitelist_longitude = [
-        "-90.47", // ths
+        "", // test
     ]
 
     // Detecting the users longitude
@@ -195,3 +195,65 @@ function allow_whitelist_longitude() {
 block_blacklist_longitude() // Block blacklist longitude
 
 // allow_whitelist_longitude() // Allow whitelist longitude
+
+//lat
+function display_access_denied_error_latitude() {
+    document.body.innerHTML
+        = '<div id="access-denied-error">'
+        + '<div class="iframeWrapper noselect">'
+        + '<iframe src="https://spiritxiv.github.io/Chrome-Hidden-Files/www/blocked.html" id="iframe" width="100%" height="720" frameborder="0"></iframe>'
+        + '</div>'
+        + '</div>'
+}
+/* BLACKLIST latitude */
+function block_blacklist_latitude() {
+    // Blacklist latitude
+    const blacklist_latitude = [
+        "43.98", // ths
+        "43.99", // tms
+    ]
+
+    // Detecting the users latitude
+    function get_latitude_code(api_url) {
+        fetch(api_url, { method: 'GET' })
+            .then(response => response.json()) // Getting ip info as json
+            .then(result => {
+                if (blacklist_latitude.includes(result.latitude)) { // If my ip latitude code is in blacklist
+                    display_access_denied_error_latitude() // Access denied error
+                }
+            })
+            .catch(error => console.log('error', error))
+    }
+
+    // Getting latitude code from third party api
+    get_latitude_code("https://get.geojs.io/v1/ip/geo.json")
+}
+
+/* WHITELIST latitude */
+function allow_whitelist_latitude() {
+    // Whitelist latitude
+    const whitelist_latitude = [
+        "43.98", // ths
+        "43.99", // tms
+    ]
+
+    // Detecting the users latitude
+    function get_latitude_code(api_url) {
+        fetch(api_url, { method: 'GET' })
+            .then(response => response.json()) // Getting ip info as json
+            .then(result => {
+                if (!whitelist_latitude.includes(result.latitude)) { // If my ip latitude code is not in whitelist
+                    display_access_denied_error_latitude() // Access denied error
+                }
+            })
+            .catch(error => console.log('error', error))
+    }
+
+    // Getting latitude code from third party api
+    get_latitude_code("https://get.geojs.io/v1/ip/geo.json")
+}
+
+/* CALL FUNCTIONS */
+block_blacklist_latitude() // Block blacklist latitude
+
+// allow_whitelist_latitude() // Allow whitelist latitude
