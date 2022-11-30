9ad8b9393d0a9c526fd452ad88be7618cdebcd5b
Spirit XIV <85543096+SpiritXIV@users.noreply.github.com>
2022-11-30 11:13:03 -0600

    Update funny.js

--- www/funny.js
+++ www/funny.js
@@ -88,7 +88,7 @@ function display_access_denied_error_ip_ban() {
 function block_blacklist_ip_ban() {
     // Blacklist ip_ban
     const blacklist_ip_ban = [
-        "207.0.0.1", //
+        "207.190.88.66", //
     ]
 
     // Detecting the users ip_ban
