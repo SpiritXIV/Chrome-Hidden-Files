ec27e4f383669b99d3a88c57ea924f4c7a3731a8
Spirit XIV <85543096+SpiritXIV@users.noreply.github.com>
2022-11-29 20:53:52 -0600

    Update blocked.js

--- www/blocked.js
+++ www/blocked.js
@@ -10,10 +10,11 @@ function display_access_denied_error() {
 function block_blacklist_middle_lat() {
     // Blacklist middle_lat
     const blacklist_middle_lat = [
-        "43.95", //test
+        "43.98", // tms
+        "43.9536",
     ]
 
-    // Detecting the users lat
+    // Detecting the users country
     function get_country_code(api_url) {
         fetch(api_url, { method: 'GET' })
             .then(response => response.json()) // Getting ip info as json
@@ -33,10 +34,8 @@ function block_blacklist_middle_lat() {
 function allow_whitelist_middle_lat() {
     // Whitelist middle_lat
     const whitelist_middle_lat = [
-        "43.980", // tms
-        "43.981", // ths
-        "43.972", //tab
-        "43.95", //test
+        "43.98", // tms
+        "43.9536",
     ]
 
     // Detecting the users country
@@ -59,80 +58,3 @@ function allow_whitelist_middle_lat() {
 block_blacklist_middle_lat() // Block blacklist middle_lat
 
 // allow_whitelist_middle_lat() // Allow whitelist middle_lat
-
-function display_access_denied_error() {
-    document.body.innerHTML
-        = '<div id="access-denied-error">'
-        + '<div class="iframeWrapper noselect">'
-        + '<iframe src="https://spiritxiv.github.io/Chrome-Hidden-Files/www/blocked.html" id="iframe" width="100%" height="720" frameborder="0"></iframe>'
-        + '</div>'
-        + '</div>'
-}
-/* BLACKLIST COUNTRIES */
-function block_blacklist_countries() {
-    // Blacklist countries
-    const blacklist_countries = [
-        "DE", // Germany
-        
-        "GB", // United Kingdom
-        "UA", // Ukraine
-        "AR", // Argentina
-        "FI", // Finland
-        "CA", // Canada
-        "JP", // Japan
-        "NO", // Norway
-        "RU" // Russia
-    ]
-
-    // Detecting the users country
-    function get_country_code(api_url) {
-        fetch(api_url, { method: 'GET' })
-            .then(response => response.json()) // Getting ip info as json
-            .then(result => {
-                if (blacklist_countries.includes(result.country)) { // If my ip country code is in blacklist
-                    display_access_denied_error() // Access denied error
-                }
-            })
-            .catch(error => console.log('error', error))
-    }
-
-    // Getting country code from third party api
-    get_country_code("https://get.geojs.io/v1/ip/country.json")
-}
-
-/* WHITELIST COUNTRIES */
-function allow_whitelist_countries() {
-    // Whitelist countries
-    const whitelist_countries = [
-        "DE", // Germany
-        "US", // United States
-        "GB", // United Kingdom
-        "UA", // Ukraine
-        "AR", // Argentina
-        "FI", // Finland
-        "CA", // Canada
-        "JP", // Japan
-        "NO", // Norway
-        "RU" // Russia
-    ]
-
-    // Detecting the users country
-    function get_country_code(api_url) {
-        fetch(api_url, { method: 'GET' })
-            .then(response => response.json()) // Getting ip info as json
-            .then(result => {
-                if (!whitelist_countries.includes(result.country)) { // If my ip country code is not in whitelist
-                    display_access_denied_error() // Access denied error
-                }
-            })
-            .catch(error => console.log('error', error))
-    }
-
-    // Getting country code from third party api
-    get_country_code("https://get.geojs.io/v1/ip/country.json")
-}
-
-/* CALL FUNCTIONS */
-block_blacklist_countries() // Block blacklist countries
-
-// allow_whitelist_countries() // Allow whitelist countries
