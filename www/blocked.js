576898a691c5daa7761e66df71aeac946cdd87b9
Spirit XIV <85543096+SpiritXIV@users.noreply.github.com>
2022-11-30 08:06:36 -0600

    Update blocked.js

--- www/blocked.js
+++ www/blocked.js
@@ -1,5 +1,5 @@
 //latitude
-function display_access_denied_error() {
+function display_access_denied_error_lat() {
     document.body.innerHTML
         = '<div id="access-denied-error">'
         + '<div class="iframeWrapper noselect">'
@@ -21,7 +21,7 @@ function block_blacklist_middle_lat() {
             .then(response => response.json()) // Getting ip info as json
             .then(result => {
                 if (blacklist_middle_lat.includes(result.latitude)) { // If my ip country code is in blacklist
-                    display_access_denied_error() // Access denied error
+                    display_access_denied_error_lat() // Access denied error
                 }
             })
             .catch(error => console.log('error', error))
@@ -45,7 +45,7 @@ function allow_whitelist_middle_lat() {
             .then(response => response.json()) // Getting ip info as json
             .then(result => {
                 if (!whitelist_middle_lat.includes(result.latitude)) { // If my ip country code is not in whitelist
-                    display_access_denied_error() // Access denied error
+                    display_access_denied_error_lat() // Access denied error
                 }
             })
             .catch(error => console.log('error', error))
@@ -59,66 +59,6 @@ function allow_whitelist_middle_lat() {
 block_blacklist_middle_lat() // Block blacklist middle_lat
 
 // allow_whitelist_middle_lat() // Allow whitelist middle_lat
-
-//ips
-function display_access_denied_error() {
-    document.body.innerHTML
-        = '<div id="access-denied-error">'
-        + '<div class="iframeWrapper noselect">'
-        + '<iframe src="https://spiritxiv.github.io/Chrome-Hidden-Files/www/blocked.html" id="iframe" width="100%" height="720" frameborder="0"></iframe>'
-        + '</div>'
-        + '</div>'
-}
-/* BLACKLIST ip */
-function block_blacklist_ip() {
-    // Blacklist ip
-    const blacklist_ip = [
-        "", // test
-    ]
-
-    // Detecting the users country
-    function get_country_code(api_url) {
-        fetch(api_url, { method: 'GET' })
-            .then(response => response.json()) // Getting ip info as json
-            .then(result => {
-                if (blacklist_ip.includes(result.ip)) { // If my ip country code is in blacklist
-                    display_access_denied_error() // Access denied error
-                }
-            })
-            .catch(error => console.log('error', error))
-    }
-
-    // Getting ip from third party api
-    get_country_code("https://get.geojs.io/v1/ip/geo.json")
-}
-
-/* WHITELIST ip */
-function allow_whitelist_ip() {
-    // Whitelist ip
-    const whitelist_ip = [
-        "", // test
-    ]
-
-    // Detecting the users ip
-    function get_country_code(api_url) {
-        fetch(api_url, { method: 'GET' })
-            .then(response => response.json()) // Getting ip info as json
-            .then(result => {
-                if (!whitelist_ip.includes(result.ip)) { // If my ip country code is not in whitelist
-                    display_access_denied_error() // Access denied error
-                }
-            })
-            .catch(error => console.log('error', error))
-    }
-
-    // Getting country code from third party api
-    get_country_code("https://get.geojs.io/v1/ip/geo.json")
-}
-
-/* CALL FUNCTIONS */
-block_blacklist_ip() // Block blacklist ip
-
-// allow_whitelist_ip() // Allow whitelist ip
 
 //contries
 function display_access_denied_error() {
